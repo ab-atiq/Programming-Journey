3c9c922b312f5feae09d61ebde16754308189c44
ab-atiq <abulbasharatiqur@gmail.com>
2026-05-24 19:38:02 +0600

    debug

--- a.c
+++ a.c
@@ -4,10 +4,20 @@ ________________________
 |     AB Atiq          |
 |______________________|
 */
-// #include <stdio.h>
-// int main()
-// {
-//     int n;
-//     printf("Hello world");
-//     return 0;
-// }
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Hello world");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
--- a.cpp
+++ a.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 int main()
 {
-    long long int a, b, c;
-    cin >> a >> b >> c;
-    cout<<max(a, max(b,c))<<endl;
-    cout<<max({a,b,c})<<endl;
+    // long long int a, b, c;
+    // cin >> a >> b >> c;
+    // cout<<max(a, max(b,c))<<endl;
+    // cout<<max({a,b,c})<<endl;
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
