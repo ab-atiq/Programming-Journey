67c47d7a98f461c11199a86112237c7706f43b75
ab-atiq <abulbasharatiqur@gmail.com>
2026-07-02 21:55:16 +0600

    DS W2C2

--- C-5-SDT/OOP_and_Python_Programming/M4_Ass_1/P_Minimize_Number.py
+++ C-5-SDT/OOP_and_Python_Programming/M4_Ass_1/P_Minimize_Number.py
@@ -44,3 +44,25 @@ while flag:
         flag = False
         
 print(cnt)
+
+
+# optimized solution
+n = int(input())
+arr = list(map(int,input().split()))
+
+operation = 0
+flag = True
+while flag:
+    num_of_div = 0
+    for i in range(0,n):
+        if arr[i] % 2 == 0:
+            arr[i] = arr[i] / 2 
+            num_of_div = num_of_div + 1
+        else:
+            break
+    if num_of_div == n:
+        operation += 1
+    else:
+        flag = False
+
+print(operation)
